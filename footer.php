61987f8a9ed8333fde6dc13241c80e20510eb9d5
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-15 14:25:12 +0100

    Modification du pied de page

--- footer.php
+++ footer.php
@@ -17,62 +17,14 @@
                                 <li><i class="fa fa-phone pr-10 text-default"></i> +00 1234567890</li>
                                 <li><a href="mailto:info@theproject.com"><i class="fa fa-envelope-o pr-10"></i>info@theproject.com</a></li>
                             </ul>
-                            <a class="collapsed map-show btn-lg-link padding-hor-clear" data-toggle="collapse" href="#collapseContact" aria-expanded="false" aria-controls="collapseContact"><i class="fa fa-envelope pr-20"></i>Contact Me</a>
                         </div>
                     </div>
                     <div class="col-sm-6 col-md-3 col-lg-4 col-lg-offset-1">
                         <div class="footer-content">
-                            <h2 class="title">Follow Me</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro fugiat accusantium similique modi alias consectetur nesciunt.</p>
-                            <ul class="social-links large circle animated-effect-1">
-                                <li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
-                                <li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-                                <li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
-                                <li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
-                                <li class="xing"><a target="_blank" href="http://www.xing.com"><i class="fa fa-xing"></i></a></li>
-                            </ul>
+                            <h2 class="title">Qui sommes-nous ?</h2>
+                            <p></p>
                         </div>
                     </div>
-                </div>
-                <div class="row">
-                    <div class="clearfix visible-sm"></div>
-                    <section id="collapseContact" class="collapse">
-                        <div class="col-md-10 col-md-offset-1">
-                            <div class="footer-content">
-                                <h2 class="title">Contact Me</h2>
-                                <div class="alert alert-success hidden" id="MessageSent2">
-                                    We have received your message, we will contact you very soon.
-                                </div>
-                                <div class="alert alert-danger hidden" id="MessageNotSent2">
-                                    Oops! Something went wrong please refresh the page and try again.
-                                </div>
-                                <form role="form" id="footer-form" class="margin-clear">
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="form-group has-feedback mb-10">
-                                                <label class="sr-only" for="name2">Name</label>
-                                                <input type="text" class="form-control" id="name2" placeholder="Name" name="name2">
-                                                <i class="fa fa-user form-control-feedback"></i>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group has-feedback mb-10">
-                                                <label class="sr-only" for="email2">Email address</label>
-                                                <input type="email" class="form-control" id="email2" placeholder="Enter email" name="email2">
-                                                <i class="fa fa-envelope form-control-feedback"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group has-feedback mb-10">
-                                        <label class="sr-only" for="message2">Message</label>
-                                        <textarea class="form-control" rows="4" id="message2" placeholder="Message" name="message2"></textarea>
-                                        <i class="fa fa-pencil form-control-feedback"></i>
-                                    </div>
-                                    <input type="submit" value="Send" class="margin-clear submit-button radius-50 btn btn-default">
-                                </form>
-                            </div>
-                        </div>
-                    </section>
                 </div>
             </div>
         </div>
