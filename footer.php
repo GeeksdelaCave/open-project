41492c5a91bdb236acedb96cb58ad9eac3c8534c
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-15 13:36:23 +0100

    Mise à jour du pied de page

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
                         <div class="footer-content">
                             <h2 class="title">Open-Project</h2>
                             <ul class="list-icons">
-                                <li><i class="fa fa-map-marker pr-10 text-default"></i> 413 avenue Gaston Berger 13625 Aix-en-Provence CEDEX 1</li>
+                                <li><i class="fa fa-map-marker pr-10 text-default"></i> 413 avenue Gaston Berger 13625 Aix-en-Provence</li>
                                 <li><i class="fa fa-phone pr-10 text-default"></i> +00 1234567890</li>
                                 <li><a href="mailto:info@theproject.com"><i class="fa fa-envelope-o pr-10"></i>info@theproject.com</a></li>
                             </ul>
