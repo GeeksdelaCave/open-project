5fefb8bfcb8825e1d9a2a8395b64d141a45efde7
GeeksdelaCave <geeksdelacave@laposte.net>
2016-12-15 10:33:05 +0100

    Création du footer

---
+++ footer.php
@@ -0,0 +1,138 @@
+
+<!-- footer start (Add "dark" class to #footer in order to enable dark footer) -->
+<!-- ================ -->
+<footer id="footer" class="clearfix dark">
+
+    <!-- .footer start -->
+    <!-- ================ -->
+    <div class="footer">
+        <div class="container">
+            <div class="footer-inner">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 col-lg-4 col-lg-offset-1">
+                        <div class="footer-content">
+                            <h2 class="title">Open-Project</h2>
+                            <ul class="list-icons">
+                                <li><i class="fa fa-map-marker pr-10 text-default"></i> 413 avenue Gaston Berger 13625 Aix-en-Provence CEDEX 1</li>
+                                <li><i class="fa fa-phone pr-10 text-default"></i> +00 1234567890</li>
+                                <li><a href="mailto:info@theproject.com"><i class="fa fa-envelope-o pr-10"></i>info@theproject.com</a></li>
+                            </ul>
+                            <a class="collapsed map-show btn-lg-link padding-hor-clear" data-toggle="collapse" href="#collapseContact" aria-expanded="false" aria-controls="collapseContact"><i class="fa fa-envelope pr-20"></i>Contact Me</a>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-3 col-lg-4 col-lg-offset-1">
+                        <div class="footer-content">
+                            <h2 class="title">Follow Me</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro fugiat accusantium similique modi alias consectetur nesciunt.</p>
+                            <ul class="social-links large circle animated-effect-1">
+                                <li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
+                                <li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
+                                <li class="googleplus"><a target="_blank" href="http://plus.google.com"><i class="fa fa-google-plus"></i></a></li>
+                                <li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+                                <li class="xing"><a target="_blank" href="http://www.xing.com"><i class="fa fa-xing"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="clearfix visible-sm"></div>
+                    <section id="collapseContact" class="collapse">
+                        <div class="col-md-10 col-md-offset-1">
+                            <div class="footer-content">
+                                <h2 class="title">Contact Me</h2>
+                                <div class="alert alert-success hidden" id="MessageSent2">
+                                    We have received your message, we will contact you very soon.
+                                </div>
+                                <div class="alert alert-danger hidden" id="MessageNotSent2">
+                                    Oops! Something went wrong please refresh the page and try again.
+                                </div>
+                                <form role="form" id="footer-form" class="margin-clear">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <div class="form-group has-feedback mb-10">
+                                                <label class="sr-only" for="name2">Name</label>
+                                                <input type="text" class="form-control" id="name2" placeholder="Name" name="name2">
+                                                <i class="fa fa-user form-control-feedback"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="form-group has-feedback mb-10">
+                                                <label class="sr-only" for="email2">Email address</label>
+                                                <input type="email" class="form-control" id="email2" placeholder="Enter email" name="email2">
+                                                <i class="fa fa-envelope form-control-feedback"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group has-feedback mb-10">
+                                        <label class="sr-only" for="message2">Message</label>
+                                        <textarea class="form-control" rows="4" id="message2" placeholder="Message" name="message2"></textarea>
+                                        <i class="fa fa-pencil form-control-feedback"></i>
+                                    </div>
+                                    <input type="submit" value="Send" class="margin-clear submit-button radius-50 btn btn-default">
+                                </form>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- .footer end -->
+
+    <!-- .subfooter start -->
+    <!-- ================ -->
+    <div class="subfooter">
+        <div class="container">
+            <div class="subfooter-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <p class="text-center">Copyright © 2016 Open-Project by <a target="_blank" href="http://htmlcoder.me">the Open-Project Team</a>. Tous droits réservés.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- .subfooter end -->
+
+</footer>
+<!-- footer end -->
+
+</div>
+<!-- page-wrapper end -->
+
+<!-- JavaScript files placed at the end of the document so the pages load faster -->
+<!-- ================================================== -->
+<!-- Jquery and Bootstap core js files -->
+<script type="text/javascript" src="plugins/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Modernizr javascript -->
+<script type="text/javascript" src="plugins/modernizr.js"></script>
+<!-- jQuery Revolution Slider  -->
+<script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="plugins/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+<!-- Isotope javascript -->
+<script type="text/javascript" src="plugins/isotope/isotope.pkgd.min.js"></script>
+<!-- Magnific Popup javascript -->
+<script type="text/javascript" src="plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<!-- Appear javascript -->
+<script type="text/javascript" src="plugins/waypoints/jquery.waypoints.min.js"></script>
+<!-- Count To javascript -->
+<script type="text/javascript" src="plugins/jquery.countTo.js"></script>
+<!-- Parallax javascript -->
+<script src="plugins/jquery.parallax-1.1.3.js"></script>
+<!-- Contact form -->
+<script src="plugins/jquery.validate.js"></script>
+<!-- Background Video -->
+<script src="plugins/vide/jquery.vide.js"></script>
+<!-- Owl carousel javascript -->
+<script type="text/javascript" src="plugins/owl-carousel/owl.carousel.js"></script>
+<!-- SmoothScroll javascript -->
+<script type="text/javascript" src="plugins/jquery.browser.js"></script>
+<script type="text/javascript" src="plugins/SmoothScroll.js"></script>
+<!-- Initialization of Plugins -->
+<script type="text/javascript" src="js/template.js"></script>
+<!-- Custom Scripts -->
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
